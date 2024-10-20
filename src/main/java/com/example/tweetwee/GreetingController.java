d89047b182d331c5c59ccb818675b9e4d7a1461d
Hotimpulse <hotimpulse@hotmail.com>
2024-10-20 20:11:26 +0300

    feat: adding filtering by tags and adjusting paths

--- src/main/java/com/example/tweetwee/GreetingController.java
+++ src/main/java/com/example/tweetwee/GreetingController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.List;
 import java.util.Map;
 
 @Controller
@@ -15,20 +16,19 @@ public class GreetingController {
     @Autowired
     private MessageRepo messageRepo;
 
-    @GetMapping("/greeting")
-    public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
-        model.addAttribute("name", name);
+    @GetMapping("/")
+    public String greeting(Map<String, Object> model) {
         return "greeting";
     }
 
-    @GetMapping
+    @GetMapping("/main")
     public String main_page(Map<String, Object> model) {
         Iterable<Message> messages = messageRepo.findAll();
         model.put("messages", messages);
         return "main";
     }
 
-    @PostMapping
+    @PostMapping("/main")
     public String add(@RequestParam String text,
                       @RequestParam String tag, Map<String, Object> model) {
         Message message = new Message(text, tag);
@@ -37,6 +37,20 @@ public class GreetingController {
         Iterable<Message> messages = messageRepo.findAll();
         model.put("messages", messages);
 
+        return "main";
+    }
+
+    @PostMapping("filter")
+    public String filter(@RequestParam String filter,
+                         Map<String, Object> model) {
+        Iterable<Message> messages;
+        if (filter != null && !filter.isEmpty()) {
+            messages = messageRepo.findByTag(filter);
+        } else {
+            messages = messageRepo.findAll();
+        }
+
+        model.put("messages", messages);
         return "main";
     }
 }
--- src/main/java/com/example/tweetwee/repos/MessageRepo.java
+++ src/main/java/com/example/tweetwee/repos/MessageRepo.java
@@ -2,5 +2,9 @@ package com.example.tweetwee.repos;
 import com.example.tweetwee.domain.Message;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface MessageRepo extends CrudRepository<Message, Long> {
+
+    List<Message> findByTag(String tag);
 }
